3e840a8ee69df89de36b2c2de0aea3e42b3bfced
thebelin <thebelin@gmail.com>
2020-06-09 10:49:17 -0600

    Save purpleprinter config, no offsets this time

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1096,11 +1096,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 225
+#define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -5
-#define Y_MIN_POS -18
+#define Y_MIN_POS -5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
