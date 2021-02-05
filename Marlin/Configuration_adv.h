9e41dd8ab27ad537596a2bcb482d143b6ac1b4f6
thebelin <thebelin@gmail.com>
2021-02-05 11:41:17 -0700

    New adv config directives

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1701,7 +1701,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_PULSE 1
+#define MINIMUM_STEPPER_PULSE 0
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
